000496f8dac33403324c83646f6533f348a3273b
portbound <prtbnd@proton.me>
2025-05-16 23:49:11 -0400

    added spinner (removed streaming)

--- api/client.go
+++ api/client.go
@@ -1,11 +1,8 @@
 package api
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/json"
-	"fmt"
-	"io"
 	"net/http"
 	"strings"
 )
@@ -16,59 +13,22 @@ type Client struct {
 
 var fullReply strings.Builder
 
-func (c *Client) HandleRequest(req *Chat) (strings.Builder, error) {
-	jsonBody, err := json.Marshal(req)
+func (c *Client) HandleRequest(chat *Chat) (*http.Response, error) {
+	jsonBody, err := json.Marshal(chat)
 	if err != nil {
-		return fullReply, err
+		return nil, err
 	}
 
 	httpReq, err := http.NewRequest(http.MethodPost, c.BaseURL, bytes.NewReader(jsonBody))
 	if err != nil {
-		return fullReply, err
+		return nil, err
 	}
 	httpReq.Header.Set("Content-Type", "application/json")
 
 	resp, err := http.DefaultClient.Do(httpReq)
 	if err != nil {
-		return fullReply, err
-	}
-	defer resp.Body.Close()
-
-	// Print User message again
-	fmt.Println(">> user:")
-	fmt.Println(req.Messages[len(req.Messages)-1].Content)
-
-	fmt.Printf("\n>> %s:\n", req.Model)
-
-	reader := bufio.NewReader(resp.Body)
-	for {
-		line, err := reader.ReadBytes('\n')
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			return fullReply, err
-		}
-
-		var chunk struct {
-			Message Message `json:"message"`
-			Done    bool    `json:"done"`
-		}
-
-		err = json.Unmarshal(line, &chunk)
-		if err != nil {
-			return fullReply, fmt.Errorf("error parsing stream chunk: %w\nChunk: %s", err, line)
-		}
-
-		fullReply.WriteString(chunk.Message.Content)
-		fmt.Print(chunk.Message.Content)
-
-		if chunk.Done {
-			break
-		}
+		return nil, err
 	}
 
-	fmt.Println()
-	fmt.Println()
-	return fullReply, nil
+	return resp, nil
 }
--- main.go
+++ main.go
@@ -1,36 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 
 	"github.com/charmbracelet/huh"
 	"github.com/tokenAPIguy/go-llama/api"
 	"github.com/tokenAPIguy/go-llama/forms"
+	"github.com/tokenAPIguy/go-llama/ui"
 )
 
 const url = "http://localhost:11434/api/chat"
 
 func main() {
-	chat := &api.Chat{}
+	chat := &api.Chat{Stream: false}
 	client := &api.Client{BaseURL: url}
 
 	// Check for existing chats
-	entries, err := os.ReadDir("chats")
-	//TODO: should create dir if it doesn't exist
-	if err != nil {
-		fmt.Println(err)
-	}
+	entries, err := CheckDir("chats")
 
-	// If there are existing chats, offer to resume one of them
-	if len(entries) != 0 {
-		err := forms.ResumeChat(chat, entries)
-		if err != nil {
-			fmt.Println(err)
+	var files []os.DirEntry
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			files = append(files, entry)
 		}
 	}
 
-	// If an existing chat was not resumed, create a new one
+	// offer to resume an existing chat
+	if len(files) != 0 {
+		err := forms.ResumeChat(chat, files)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	// if we didn't find any existing chats, or the user declined, start new chat
 	if chat.Name == "" {
 		err := forms.NewChat(chat)
 		if err != nil {
@@ -38,14 +46,22 @@ func main() {
 		}
 	}
 
-	// Starting REPL
+	// Start REPL
+	title := fmt.Sprintf("Prompt - %s", chat.Name)
 	var lines string
+
+	if len(chat.Messages) > 0 {
+		for _, msg := range chat.Messages {
+			fmt.Printf(">> %s:\n", msg.Role)
+			fmt.Printf("%s\n\n", msg.Content)
+		}
+	}
 
 	for {
 		form := huh.NewForm(
 			huh.NewGroup(
 				huh.NewText().
-					Title("Prompt").
+					Title(title).
 					CharLimit(10000).
 					Value(&lines),
 			),
@@ -53,27 +69,46 @@ func main() {
 		err = form.Run()
 		if err != nil {
 			fmt.Println(err)
+			return
 		}
 
 		// Build user msg
+		fmt.Println(chat.Messages)
 		chat.Messages = append(chat.Messages, api.Message{
 			Role:    "user",
 			Content: lines,
 		})
 
+		fmt.Println(chat.Messages)
 		// Send
-		chat.Stream = true
+		done := make(chan struct{})
+		var resp *http.Response
 
-		reply, err := client.HandleRequest(chat)
+		go func() {
+			resp, err = client.HandleRequest(chat)
+			close(done)
+		}()
+
+		ui.RunSpinnerUntil(done)
+
+		fmt.Println(">> user:")
+		fmt.Printf("%s\n\n", chat.Messages[len(chat.Messages)-1].Content)
+
+		fullReply, err := renderResponse(resp)
 		if err != nil {
-			fmt.Println("Failed to handle client request:", err)
+			fmt.Println(err)
+			return
 		}
 
-		// Build assistant msg
 		chat.Messages = append(chat.Messages, api.Message{
 			Role:    "assistant",
-			Content: reply.String(),
+			Content: fullReply,
 		})
+
+		fmt.Println(">> assistant:")
+		fmt.Printf("%s\n\n", fullReply)
+
+		// TODO: figure out why it's duplicating existing files with .json, WriteFile should create only if it doesnt exist...
 
 		err = chat.SaveChat()
 		if err != nil {
@@ -85,4 +120,21 @@ func main() {
 	}
 }
 
-//TODO: dynamically pull the available models
+func renderResponse(resp *http.Response) (string, error) {
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", nil
+	}
+
+	var result struct {
+		Message api.Message `json:"message"`
+	}
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return "", err
+	}
+
+	return result.Message.Content, nil
+}
